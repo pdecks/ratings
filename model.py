001a2e23ecea53f8ea478c003be76e56255a1dfd
Hackbright Student <info@hackbrightacademy.com>
2015-10-22 17:39:53 -0700

    updated database with entries for erica

--- model.py
+++ model.py
@@ -29,6 +29,27 @@ class User(db.Model):
 
         return "<User user_id=%s email=%s>" % (self.user_id, self.email)
 
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0
+
+
 
 # Put your Movie and Rating model classes here.
 class Movie(db.Model):
